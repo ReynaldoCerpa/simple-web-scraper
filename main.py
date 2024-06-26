8d4ec55c1930a38e40a11aba0bf81523504b5ecd
Reynaldo Cerpa <reynaldocerpa07@gmail.com>
2024-06-26 15:18:19 -0700

    add more info rows

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def initialize_driver():
     driver = webdriver.Chrome(options=options)
     return driver
 
-def get_vendor_info(driver):
+def get_info(driver):
     """Extract vendor name and number of reviews."""
     name_element = driver.find_element(By.CLASS_NAME, 'Lhccdd')
     raw_business_name = name_element.text.split('\n')[0]
@@ -47,15 +47,29 @@ def extract_reviews(driver, num_reviews):
                     comment_element = container.find_element(By.CLASS_NAME, 'Jtu6Td')
 
                 driver.execute_script("arguments[0].scrollIntoView(true);", container)
-                time.sleep(0.2)
+                time.sleep(0.7)
                 user = user_element.text
                 comment = 'Sin comentarios' if not comment_element.text or comment_element.text == 'Más' else comment_element.text
-                reviews.append({'user': user, 'comment': comment})
+                # date: dehysf lTi8oc
+                date_published = container.find_element(By.CLASS_NAME, 'dehysf').text
+                # images: DQBZx
+                number_of_images = 0
+                try:
+                    images_container = container.find_element(By.CLASS_NAME, 'EDblX ')
+                except:
+                    pass
+                else:
+                    # number of images: s2gQvd
+                    number_of_images = len(images_container.find_elements(By.TAG_NAME, 'a'))
+                # stars: lTi8oc z3HNkc
+                number_of_stars = container.find_element(By.CLASS_NAME, 'z3HNkc')
+                stars = number_of_stars.get_attribute('aria-label')
+                reviews.append({'user': user, 'comment': comment, 'date': date_published, 'images': number_of_images, 'stars': stars})
             except Exception as e:
                 print(f"Error extracting review: {e}")
 
         offset = len(review_containers)
-        time.sleep(0.2)
+        time.sleep(.7)
     return reviews
 
 def save_reviews_to_csv(raw_business_name, reviews):
@@ -64,16 +78,17 @@ def save_reviews_to_csv(raw_business_name, reviews):
     filename = f'output/{sanitized_business_name}.csv'
     with open(filename, 'w', encoding='utf-8') as output:
         writer = csv.writer(output)
+        writer.writerow(['usuario', 'Comentario', 'Fecha', '# Imagenes', 'Calificacion'])
         for item in reviews:
-            writer.writerow([item['user'], item['comment']])
+            writer.writerow([item['user'], item['comment'], item['date'], item['images'], item['stars']])
 
 def scrape_google_reviews(url):
     """Main function to scrape Google Reviews."""
     driver = initialize_driver()
     driver.get(url)
-    time.sleep(1)
+    time.sleep(2)
 
-    raw_business_name, num_reviews = get_vendor_info(driver)
+    raw_business_name, num_reviews = get_info(driver)
     reviews = extract_reviews(driver, num_reviews)
 
     driver.quit()
@@ -81,7 +96,7 @@ def scrape_google_reviews(url):
     return reviews
 
 if __name__ == '__main__':
-    url = input('Enter URL: ')
-    #url = 'https://www.google.com/search?q=cafe+vainilla&oq=cafe+vainilla&gs_lcrp=EgZjaHJvbWUqBggAEEUYOzIGCAAQRRg7MgYIARBFGD0yBggCEC4YQNIBCDE1MjFqMGoxqAIAsAIA&sourceid=chrome&ie=UTF-8#lrd=0x80d88fc9ecbe05e9:0x55c3e664040f1fb7,1,,,,'
+    #url = input('Enter URL: ')
+    url = 'https://www.google.com/search?q=cafe+vainilla&oq=cafe+vainilla&gs_lcrp=EgZjaHJvbWUqBggAEEUYOzIGCAAQRRg7MgYIARBFGD0yBggCEC4YQNIBCDE1MjFqMGoxqAIAsAIA&sourceid=chrome&ie=UTF-8#lrd=0x80d88fc9ecbe05e9:0x55c3e664040f1fb7,1,,,,'
     reviews = scrape_google_reviews(url)
     print(f'\nFinished successfully, scraped {len(reviews)} reviews')
