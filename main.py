f2b01b9b3c974234a314497d6fe2354da0332eb5
Reynaldo Cerpa <reynaldocerpa07@gmail.com>
2024-06-26 15:54:38 -0700

    detects language

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import langid
 import csv
 import time
 import re
@@ -18,6 +19,16 @@ def get_info(driver):
     review_count_element = driver.find_element(By.CLASS_NAME, 'z5jxId')
     num_reviews = int(review_count_element.text.split(' ')[0])
     return raw_business_name, num_reviews
+
+def detect_language_with_langid(line): 
+    pred = langid.classify(line)
+    if pred[0] == 'es':
+        return 'español'
+    if pred[0] == 'en':
+        return 'ingles'
+    else:
+        return 'español'
+
 
 def extract_reviews(driver, num_reviews):
     """Extract reviews from the Google Reviews page."""
@@ -41,15 +52,15 @@ def extract_reviews(driver, num_reviews):
                     pass
 
                 try:
-                    # review-more-link
                     comment_element = container.find_element(By.CLASS_NAME, 'f5axBf')
                 except:
                     comment_element = container.find_element(By.CLASS_NAME, 'Jtu6Td')
 
                 driver.execute_script("arguments[0].scrollIntoView(true);", container)
-                time.sleep(0.7)
+                time.sleep(0.3)
                 user = user_element.text
                 comment = 'Sin comentarios' if not comment_element.text or comment_element.text == 'Más' else comment_element.text
+                lang = detect_language_with_langid(comment)
                 # date: dehysf lTi8oc
                 date_published = container.find_element(By.CLASS_NAME, 'dehysf').text
                 # images: DQBZx
@@ -64,12 +75,12 @@ def extract_reviews(driver, num_reviews):
                 # stars: lTi8oc z3HNkc
                 number_of_stars = container.find_element(By.CLASS_NAME, 'z3HNkc')
                 stars = number_of_stars.get_attribute('aria-label')
-                reviews.append({'user': user, 'comment': comment, 'date': date_published, 'images': number_of_images, 'stars': stars})
+                reviews.append({'user': user, 'comment': comment, 'date': date_published, 'images': number_of_images, 'stars': stars, 'lang': lang})
             except Exception as e:
                 print(f"Error extracting review: {e}")
 
         offset = len(review_containers)
-        time.sleep(.7)
+        time.sleep(.2)
     return reviews
 
 def save_reviews_to_csv(raw_business_name, reviews):
@@ -78,15 +89,15 @@ def save_reviews_to_csv(raw_business_name, reviews):
     filename = f'output/{sanitized_business_name}.csv'
     with open(filename, 'w', encoding='utf-8') as output:
         writer = csv.writer(output)
-        writer.writerow(['usuario', 'Comentario', 'Fecha', '# Imagenes', 'Calificacion'])
+        writer.writerow(['usuario', 'Comentario', 'Fecha', '# Imagenes', 'Calificacion', 'Idioma'])
         for item in reviews:
-            writer.writerow([item['user'], item['comment'], item['date'], item['images'], item['stars']])
+            writer.writerow([item['user'], item['comment'], item['date'], item['images'], item['stars'], item['lang']])
 
 def scrape_google_reviews(url):
     """Main function to scrape Google Reviews."""
     driver = initialize_driver()
     driver.get(url)
-    time.sleep(2)
+    time.sleep(.5)
 
     raw_business_name, num_reviews = get_info(driver)
     reviews = extract_reviews(driver, num_reviews)
