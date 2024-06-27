1674bc47a7d2db7ef94e384645659fd6e8c8d641
Reynaldo Cerpa <reynaldocerpa07@gmail.com>
2024-06-26 18:27:18 -0700

    fix, long reviews stopped scrolldown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import langid
 import csv
 import time
@@ -17,8 +18,10 @@ def get_info(driver):
     name_element = driver.find_element(By.CLASS_NAME, 'Lhccdd')
     raw_business_name = name_element.text.split('\n')[0]
     review_count_element = driver.find_element(By.CLASS_NAME, 'z5jxId')
-    num_reviews = int(review_count_element.text.split(' ')[0])
-    return raw_business_name, num_reviews
+    sanitized_num_reviews = re.sub(r'\D', '', review_count_element.text.split(' ')[0])
+    # Remove any blank spaces
+    num_reviews = sanitized_num_reviews.replace(' ', '')
+    return raw_business_name, int(num_reviews)
 
 def detect_language_with_langid(line): 
     pred = langid.classify(line)
@@ -37,6 +40,7 @@ def extract_reviews(driver, num_reviews):
 
     while len(reviews) < num_reviews:
         review_containers = driver.find_elements(By.CLASS_NAME, 'gws-localreviews__google-review')
+        print(offset, len(review_containers))
         if offset == len(review_containers):
             break
 
@@ -57,7 +61,7 @@ def extract_reviews(driver, num_reviews):
                     comment_element = container.find_element(By.CLASS_NAME, 'Jtu6Td')
 
                 driver.execute_script("arguments[0].scrollIntoView(true);", container)
-                time.sleep(0.3)
+                time.sleep(.2)
                 user = user_element.text
                 comment = 'Sin comentarios' if not comment_element.text or comment_element.text == 'Más' else comment_element.text
                 lang = detect_language_with_langid(comment)
@@ -80,7 +84,9 @@ def extract_reviews(driver, num_reviews):
                 print(f"Error extracting review: {e}")
 
         offset = len(review_containers)
-        time.sleep(.2)
+        scrollable_div = driver.find_element(By.CLASS_NAME, 'review-dialog-list')
+        driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", scrollable_div)
+        time.sleep(1)
     return reviews
 
 def save_reviews_to_csv(raw_business_name, reviews):
@@ -97,7 +103,7 @@ def scrape_google_reviews(url):
     """Main function to scrape Google Reviews."""
     driver = initialize_driver()
     driver.get(url)
-    time.sleep(.5)
+    time.sleep(1)
 
     raw_business_name, num_reviews = get_info(driver)
     reviews = extract_reviews(driver, num_reviews)
@@ -107,7 +113,7 @@ def scrape_google_reviews(url):
     return reviews
 
 if __name__ == '__main__':
-    #url = input('Enter URL: ')
-    url = 'https://www.google.com/search?q=cafe+vainilla&oq=cafe+vainilla&gs_lcrp=EgZjaHJvbWUqBggAEEUYOzIGCAAQRRg7MgYIARBFGD0yBggCEC4YQNIBCDE1MjFqMGoxqAIAsAIA&sourceid=chrome&ie=UTF-8#lrd=0x80d88fc9ecbe05e9:0x55c3e664040f1fb7,1,,,,'
+    url = input('Enter URL: ')
+    #url = 'https://www.google.com/search?q=cafe+vainilla&oq=cafe+vainilla&gs_lcrp=EgZjaHJvbWUqBggAEEUYOzIGCAAQRRg7MgYIARBFGD0yBggCEC4YQNIBCDE1MjFqMGoxqAIAsAIA&sourceid=chrome&ie=UTF-8#lrd=0x80d88fc9ecbe05e9:0x55c3e664040f1fb7,1,,,,'
     reviews = scrape_google_reviews(url)
     print(f'\nFinished successfully, scraped {len(reviews)} reviews')
